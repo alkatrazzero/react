27e64ebce4a5e2c3792d719e0d2d153577fd015b
mark <mark@macbook-air-mark.local>
2021-03-20 16:18:51 +0300

    add project

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.css';
+import Header from "./componnents/Header";
+import Nav from "./componnents/Nav";
+import Profile from "./componnents/Profile";
+
+
+const App = () => {
+    return (<div className='app-wrapper'>
+            <Header/>
+            <Nav/>
+            <Profile/>
+        </div>
+    );
+}
+export default App;
