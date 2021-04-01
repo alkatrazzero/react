9ead6cef97c9a167f1bf16db3e9a675e1b4f40ed
alkatrazzero <vibranovskypython@gmail.com>
2021-04-01 22:55:34 +0300

    first-commit-form-vscode

--- src/App.js
+++ src/App.js
@@ -1,17 +1,16 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import "./App.css";
 import Header from "./componnents/Header";
 import Nav from "./componnents/Nav";
 import Profile from "./componnents/Profile";
 
-
 const App = () => {
-    return (<div className='app-wrapper'>
-            <Header/>
-            <Nav/>
-            <Profile/>
-        </div>
-
-    );
-}
+  return (
+    <div className="app-wrapper">
+      <Header />
+      <Nav />
+      <Profile />
+    </div>
+  );
+};
 export default App;
