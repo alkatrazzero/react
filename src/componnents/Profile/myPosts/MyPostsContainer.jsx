a86e84b9bced31cb6133e3f6fb27ed75fce5848b
alkatrazzero <vibranovskypython@gmail.com>
2021-04-25 21:53:35 +0300

    react_redux lybrary

--- src/componnents/Profile/myPosts/MyPostsContainer.jsx
+++ src/componnents/Profile/myPosts/MyPostsContainer.jsx
@@ -1,26 +1,45 @@
 import React from "react";
+import { connect } from "react-redux";
 import {
   addPostActionCreator,
   updateNewPostTextActionCreator,
 } from "../../../redux/profileReducer";
 import MyPosts from "./MyPosts";
 
-const MyPostsConteiner = (props) => {
-  let state = props.store.getState();
-  let addPostNew = () => {
-    props.store.dispatch(addPostActionCreator());
+// const MyPostsConteiner = (props) => {
+//   let state = props.store.getState();
+//   let addPostNew = () => {
+//     props.store.dispatch(addPostActionCreator());
+//   };
+//   let onChangePost = (text) => {
+//     props.store.dispatch(updateNewPostTextActionCreator(text));
+//   };
+//   return (
+//     <MyPosts
+//       updateNewPostText={onChangePost}
+//       addPost={addPostNew}
+//       posts={state.profilePage.posts}
+//       newPostText={state.profilePage.newPostText}
+//     />
+//   );
+// };
+const mapStateToProps = (state) => {
+  return {
+    posts: state.profilePage.posts,
+    newPostText: state.profilePage.newPostText,
   };
-  let onChangePost = (text) => {
-    let action = updateNewPostTextActionCreator(text);
-    props.store.dispatch(action);
-  };
-  return (
-    <MyPosts
-      updateNewPostText={onChangePost}
-      addPost={addPostNew}
-      posts={state.profilePage.posts}
-      newPostText={state.profilePage.newPostText}
-    />
-  );
 };
+const mapDispatchToProps = (dispatch) => {
+  return {
+    addPost: () => {
+      dispatch(addPostActionCreator());
+    },
+    updateNewPostText: (text) => {
+      let action = updateNewPostTextActionCreator(text);
+      dispatch(action);
+    },
+  };
+};
+const MyPostsConteiner = connect(mapStateToProps, mapDispatchToProps)(MyPosts);
+
 export default MyPostsConteiner;
--- src/index.js
+++ src/index.js
@@ -5,15 +5,18 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import { BrowserRouter } from "react-router-dom";
+import { Provider } from "react-redux";
 let rerenderEntireTree = (state) => {
   ReactDOM.render(
     <BrowserRouter>
       <React.StrictMode>
-        <App
-          state={state}
-          dispatch={store.dispatch.bind(store)}
-          store={store}
-        />
+        <Provider store={store}>
+          <App
+            state={state}
+            dispatch={store.dispatch.bind(store)}
+            store={store}
+          />
+        </Provider>
       </React.StrictMode>
     </BrowserRouter>,
     document.getElementById("root")
